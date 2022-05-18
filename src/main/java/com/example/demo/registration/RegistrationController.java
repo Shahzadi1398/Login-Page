65ec6ab18f58caeb4117fe872ff8c3043bb57b9e
Shahzadi1398 <parveenshahzadi13@gmail.com>
2022-05-18 09:53:15 +0530

    first

---
+++ src/main/java/com/example/demo/registration/RegistrationController.java
@@ -0,0 +1,22 @@
+package com.example.demo.registration;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path ="api/v1/registration")
+@AllArgsConstructor
+public class RegistrationController {
+
+    private final RegistrationService registrationService;
+    @PostMapping("/save")
+    public String register(@RequestBody RegistrationRequest request){
+        return registrationService.register(request);
+    }
+
+    @GetMapping("/confirm")
+    public String confirm(@RequestParam("token") String token){
+        return registrationService.confirmToken(token);
+    }
+}
